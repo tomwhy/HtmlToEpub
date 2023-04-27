96ca3d18d4d6c4b21bf26b6a6da8b52eca0ba1bc
Tom Why <TomWhy2@gmail.com>
2023-04-27 17:48:50 +0300

    added a css file to the ebook

--- ebook.py
+++ ebook.py
@@ -1,6 +1,7 @@
 from ebooklib import epub
 from typing import List, Union
 from dataclasses import dataclass
+from pathlib import Path
 
 @dataclass
 class Chapter:
@@ -19,14 +20,22 @@ class Ebook:
 
         if author:
             self._book.add_author(author)
-
+            
+        self._book_css = epub.EpubItem(file_name="style/ebook.css", media_type="text/css", content=Path("css/ebook.css").read_bytes())
+        self._book.add_item(self._book_css)
+        
         self._book.spine = ['nav']
         self._book.toc = []
 
-    def __create_epub_chapter(self, chapter: Chapter) -> epub.EpubHtml:
+    def __create_epub_chapter(self, chapter: Chapter, include_title: bool = True) -> epub.EpubHtml:
         chapter_filename = f"{chapter.title}.xhtml"
         epub_chapter = epub.EpubHtml(title=chapter.title, file_name=chapter_filename)
-        epub_chapter.set_content(chapter.content)
+        epub_chapter.add_item(self._book_css)
+
+        content = chapter.content
+        if include_title:
+            content = f"<div class=title>{chapter.title}</div>{content}"
+        epub_chapter.set_content(content)
 
         self._book.add_item(epub_chapter)
         return epub_chapter
@@ -35,7 +44,7 @@ class Ebook:
         epub_chapter = self.__create_epub_chapter(chapter)
 
         self._book.spine.append(epub_chapter)
-        self._book.toc.append(epub.Link(epub_chapter.file_name, chapter.title))
+        self._book.toc.append(epub.Link(epub_chapter.file_name, chapter.title, chapter.title))
 
     def add_section(self, name: str, chapters: List[Chapter]):
         epub_chapters = [self.__create_epub_chapter(c) for c in chapters]
